790e119f0b110a75daaa27344e6ba0f076648fe7
FraGioco9 <giocolifrancesco@gmail.com>
2026-07-24 22:48:41 +0200

    Clarify Flow metadata and wallet batch units

--- sitecustomize.py
+++ sitecustomize.py
@@ -3,5 +3,8 @@
 import fresh_mfl_database_rebuild as rebuild
 
 
+# Number of player IDs requested in each Flow metadata batch.
 rebuild.FLOW_BATCH_SIZE = 3000
+
+# Number of wallet addresses requested in each Flow wallet-ownership batch.
 rebuild.WALLET_BATCH_SIZE = 3000
