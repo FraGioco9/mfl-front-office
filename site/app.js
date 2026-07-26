3e16962d385a9877a064bbbde92aee03214594c4
FraGioco9 <giocolifrancesco@gmail.com>
2026-07-27 01:06:17 +0200

    Bump static loader to 1.149.73

--- site/app.js
+++ site/app.js
@@ -1,1 +1,1 @@
-document.write('<script src="/app-core.js?v=1.149.72"><\/script><script src="/app-fixes.js?v=1.149.72"><\/script>');
+document.write('<script src="/app-core.js?v=1.149.73"><\/script><script src="/app-fixes.js?v=1.149.73"><\/script>');
