f38ee62de67f78ae92718f2fd0b1c5b9616c9932
FraGioco9 <giocolifrancesco@gmail.com>
2026-07-27 03:23:41 +0200

    Bust cache for contract column widths

--- site/app.js
+++ site/app.js
@@ -1,1 +1,1 @@
-document.write('<script src="/app-core.js?v=1.149.74"><\/script><script src="/app-fixes.js?v=1.149.74"><\/script><script src="/v14974-followup-fixes.js?v=1.149.74-12"><\/script>');
+document.write('<script src="/app-core.js?v=1.149.74"><\/script><script src="/app-fixes.js?v=1.149.74"><\/script><script src="/v14974-followup-fixes.js?v=1.149.74-13"><\/script>');
