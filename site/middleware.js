8a8cfcdbf041ee35917c432d7c9a413836dccb95
FraGioco9 <giocolifrancesco@gmail.com>
2026-07-29 02:01:48 +0200

    Simplify stabilized app middleware

--- site/middleware.js
+++ site/middleware.js
@@ -2,15 +2,11 @@ export const config = {
   matcher: "/app.js",
 };
 
-export default async function middleware(request) {
+export default function middleware(request) {
   const target = new URL("/api/app-bundle", request.url);
-  const response = await fetch(target, {
+  return fetch(target, {
     headers: {
       "x-mfl-app-bundle": "1",
     },
-  });
-  return new Response(response.body, {
-    status: response.status,
-    headers: response.headers,
   });
 }
