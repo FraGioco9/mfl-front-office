198a54e7414d8ca69200317ecf1c9a9134c023c9
FraGioco9 <giocolifrancesco@gmail.com>
2026-08-07 17:26:01 +0200

    fix: version the entry-module import

--- site/app.js
+++ site/app.js
@@ -1,7 +1,13 @@
 (() => {
   "use strict";
 
-  import("./modules/app-entry.js").catch((error) => {
+  const currentScript = document.currentScript;
+  const scriptUrl = currentScript?.src ? new URL(currentScript.src, window.location.href) : null;
+  const version = scriptUrl?.searchParams.get("v") || String(Date.now());
+  const entryUrl = new URL("./modules/app-entry.js", window.location.href);
+  entryUrl.searchParams.set("v", version);
+
+  import(entryUrl.href).catch((error) => {
     console.error("Could not import the MFL Front Office entry module.", error);
   });
 })();
