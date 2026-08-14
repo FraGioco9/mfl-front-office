3b508460cc0549a36973d83d67225fb54d5a83ec
FraGioco9 <giocolifrancesco@gmail.com>
2026-08-14 19:30:42 +0200

    Revert broken dropdown rebuild

--- site/bootstrap.js
+++ site/bootstrap.js
@@ -292,7 +292,6 @@
   }
 
   syncBootstrapFirstPaint();
-  loadBootstrapRuntime("/dropdowns-runtime.js");
   loadBootstrapRuntime("/club-squad-route-runtime.js");
   loadBootstrapRuntime("/filter-controls-runtime.js");
 
