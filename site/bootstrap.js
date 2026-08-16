46c2ec70ddc06c7fb0dcb41bdee0fc5292ee1a2e
FraGioco9 <giocolifrancesco@gmail.com>
2026-08-16 23:46:41 +0200

    Release v1.124.40

--- site/bootstrap.js
+++ site/bootstrap.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const STATIC_RELEASE_VERSION = "1.124.39";
+  const STATIC_RELEASE_VERSION = "1.124.40";
   window.__mflReleaseVersion = STATIC_RELEASE_VERSION;
 
   document.documentElement.classList.add("mflSingleRenderPending", "mflInitialRouteResolved");
