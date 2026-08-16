ecf05d78586221d88f2386090aabb67a01433eee
FraGioco9 <giocolifrancesco@gmail.com>
2026-08-16 12:49:52 +0200

    Set bootstrap release to v1.124.7

--- site/bootstrap.js
+++ site/bootstrap.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const STATIC_RELEASE_VERSION = "1.124.6";
+  const STATIC_RELEASE_VERSION = "1.124.7";
   window.__mflReleaseVersion = STATIC_RELEASE_VERSION;
 
   document.documentElement.classList.add("mflSingleRenderPending", "mflInitialRouteResolved");
