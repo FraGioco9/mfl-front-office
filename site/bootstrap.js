fcf0eea62048924ae831d9bf5091b87a26d15216
FraGioco9 <giocolifrancesco@gmail.com>
2026-08-16 23:33:15 +0200

    Align bootstrap with v1.124.39

--- site/bootstrap.js
+++ site/bootstrap.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const STATIC_RELEASE_VERSION = "1.124.38";
+  const STATIC_RELEASE_VERSION = "1.124.39";
   window.__mflReleaseVersion = STATIC_RELEASE_VERSION;
 
   document.documentElement.classList.add("mflSingleRenderPending", "mflInitialRouteResolved");
