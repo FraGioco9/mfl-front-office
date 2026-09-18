be831be787b8c724c4e4c2850ccf05435bddfbbe
FraGioco9 <giocolifrancesco@gmail.com>
2026-09-18 22:59:23 +0200

    Render Planner title in initial Next response

--- pages/[...path].js
+++ pages/[...path].js
@@ -1,7 +1,25 @@
-export function getServerSideProps() {
-  return { props: {} };
+import React from "react";
+import Head from "next/head";
+
+function initialDocumentTitle(resolvedUrl = "") {
+  const pathname = String(resolvedUrl || "").split(/[?#]/, 1)[0] || "/";
+  if (pathname === "/planner") return "Planner - MFL Front Office";
+  return "";
 }
 
-export default function MflLegacyShellRoutePage() {
-  return null;
+export function getServerSideProps(context) {
+  return {
+    props: {
+      initialDocumentTitle: initialDocumentTitle(context?.resolvedUrl),
+    },
+  };
+}
+
+export default function MflLegacyShellRoutePage({ initialDocumentTitle = "" }) {
+  if (!initialDocumentTitle) return null;
+  return React.createElement(
+    Head,
+    null,
+    React.createElement("title", null, initialDocumentTitle),
+  );
 }
